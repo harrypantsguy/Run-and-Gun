3d905e4c5da976c2c665f94a56e2d041b16ef54c
harrypantsguy <harrypants2014@gmail.com>
2023-04-10 22:54:38 -0700

    - Started on tile distance counter system

--- Assets/_Project/Codebase/Gameplay/Projectile.cs
+++ Assets/_Project/Codebase/Gameplay/Projectile.cs
@@ -15,6 +15,8 @@ namespace _Project.Codebase.Gameplay
         private int m_remainingPierces;
         private int m_remainingRicochets;
         private Cell m_cellInsideOf;
+        private float m_equivalentUnitDistance;
+        private float m_tilePierceDistSum;
 
         public static float DEFAULT_SPEED = 75f;
         public static float MAX_TRAVEL_DIST = 120f;
@@ -22,7 +24,8 @@ namespace _Project.Codebase.Gameplay
 
         private void Start()
         {
-            m_remainingPierces = 1;
+            Debug.Break();
+            m_remainingPierces = 2;
             m_remainingRicochets = 1;
             
             m_lastFixedTimeCall = Time.time;
@@ -30,11 +33,14 @@ namespace _Project.Codebase.Gameplay
             float tickTravelDist = m_speed * Time.fixedDeltaTime;
             m_nextPosition = transform.position + transform.right * tickTravelDist;
             m_interpolationTime = Time.fixedDeltaTime;
+
+            m_equivalentUnitDistance = CalculateEquivalentTravelDistanceFromDirection(transform.right);
             //transform.localScale = new Vector3(tickTravelDist, transform.localScale.y, transform.localPosition.z);
         }
 
         private void LateUpdate()
         {
+            Debug.Log($"{m_tilePierceDistSum} {m_remainingPierces}");
             transform.position = Vector2.Lerp(m_oldPosition, m_nextPosition, 
                 (Time.time - m_lastFixedTimeCall) / m_interpolationTime);
         }
@@ -47,12 +53,23 @@ namespace _Project.Codebase.Gameplay
             float dist = m_speed * Time.fixedDeltaTime;
             Vector2 newPosition = transform.position + transform.right * dist;
 
+            Debug.DrawLine(transform.position, newPosition, new Color(0f, 1f, 0f, .5f));
+            
             RaycastHit2D hit = Physics2D.Linecast(transform.position, newPosition);
             if (hit.collider != null)
             {
                 Vector2 testPos = hit.point - hit.normal * .1f;
                 Cell cell = Building.building.GetWallAtPos(testPos);
-                
+
+                if (m_cellInsideOf == null)
+                    m_tilePierceDistSum += (newPosition - hit.point).magnitude;
+
+                if (m_tilePierceDistSum > m_equivalentUnitDistance)
+                {
+                    m_tilePierceDistSum -= m_equivalentUnitDistance;
+                    m_remainingPierces--;
+                }
+
                 Debug.DrawLine(hit.point, Vector3.zero, Color.red, 1f);
                 
                 if (m_cellInsideOf != cell)
@@ -62,18 +79,50 @@ namespace _Project.Codebase.Gameplay
                     bool reachedExactlyZero = false;
                     if (m_remainingPierces > 0)
                     {
-                        m_remainingPierces -= cell.pierceInfluence;
+                        m_remainingPierces -= cell.pierceInfluence; // REDETERMINE PIERCE INFLUENCE CONSTANTLY WHILE INSIDE
                         if (m_remainingPierces == 0) 
                             reachedExactlyZero = true;
                     }
 
                     if (!reachedExactlyZero && m_remainingPierces <= 0)
                     {
-                        m_nextPosition = (Vector3)hit.point - (transform.right * IMPACT_OFFSET_DIST);
+                        m_nextPosition = (Vector3)hit.point - transform.right * IMPACT_OFFSET_DIST;
                         m_interpolationTime = (m_nextPosition - (Vector2)transform.position).magnitude / m_speed;
                         Destroy(gameObject, m_interpolationTime);
                         return;
                     }
+                }
+            } 
+            else if (m_cellInsideOf != null)
+            {
+                RaycastHit2D reverseCast = Physics2D.Raycast(newPosition, -transform.right, dist);
+                
+                if (reverseCast.collider == null)
+                {
+                    Debug.DrawRay(newPosition, -transform.right, Color.yellow);
+                    m_tilePierceDistSum += dist;
+                }
+                else
+                {
+                    Debug.DrawLine(newPosition, reverseCast.point, Color.red);
+                    m_cellInsideOf = null;
+                    float additionalDistTraveledInside = dist - reverseCast.distance;
+                
+                    m_tilePierceDistSum += additionalDistTraveledInside;
+                }
+
+                if (m_tilePierceDistSum > m_equivalentUnitDistance)
+                {
+                    m_remainingPierces--; // ACCOUNT FOR PIERCE INFLUENCE
+                    m_tilePierceDistSum -= m_equivalentUnitDistance;
+                }
+
+                if (m_remainingPierces <= 0)
+                {
+                    m_nextPosition = newPosition;
+                    m_interpolationTime = (m_nextPosition - (Vector2)transform.position).magnitude / m_speed;
+                    Destroy(gameObject, m_interpolationTime);
+                    return;
                 }
             }
 
@@ -82,6 +131,15 @@ namespace _Project.Codebase.Gameplay
                 Destroy(gameObject);
             
             m_nextPosition = newPosition;
+        }
+
+        private static float CalculateEquivalentTravelDistanceFromDirection(Vector2 direction)
+        {
+            float x = direction.x;
+            float y = direction.y;
+
+            float slope = Mathf.Abs(x) > Mathf.Abs(y) ? y / x : x / y;
+            return Mathf.Sqrt(slope * slope + 1);
         }
 
         public static void SpawnProjectile(Vector2 position, Vector2 direction, float speed)
